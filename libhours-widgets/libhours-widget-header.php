96b366c8804ef2fdbf551a0f824cafbd03493be8
Dustin Younse <milsyobtaf@gmail.com>
2012-12-11 12:33:34 -0600

    shortened curl timeouts to 1s

--- libhours-widgets/libhours-widget-header.php
+++ libhours-widgets/libhours-widget-header.php
@@ -56,8 +56,8 @@ function get_content($URL){
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_URL, $URL);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 4);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 4);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 1);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
 	curl_setopt($ch, CURLOPT_FAILONERROR, true);
 	$data = curl_exec($ch);
 	curl_close($ch);
